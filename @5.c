4a8d62b106e7957312ebde13b825db369023d4a3
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2017-09-06 09:44:52 -0700

    Create @5.c

---
+++ @5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    if(n1>=n2&&n1>=n3)
+        printf("%.2fis the largest number.", n1);
+
+    if(n2>=n1&&n2>=n3)
+        printf("%.2f isthe larget number.", n2);
+
+    if(n3>=n1&&n3>=n2)
+        printf("%.2f isthe larest number.", n3);
+
+    return 0;
+}
